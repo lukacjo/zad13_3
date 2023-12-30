9cf0264c4d2c73eb1172dc38e49a970a467d0f3f
lukacjo <85407127+lukacjo@users.noreply.github.com>
2023-12-30 10:01:36 +0100

    wsadzenie wszystkiego do main

--- zad13_3.py
+++ zad13_3.py
@@ -10,74 +10,78 @@ from sqlalchemy import (
 )
 import pandas as pd
 
-engine = create_engine("sqlite:///database.db", echo=False)
 
-meta = MetaData()
-# station,date,precip,tobs
+def main():
+    engine = create_engine("sqlite:///database.db", echo=False)
 
-measure = Table(
-    "measure",
-    meta,
-    Column("id", Integer, primary_key=True),
-    Column("station", String),
-    Column("date", Date),
-    Column("precip", Float),
-    Column("tobs", Integer),
-)
-# station,latitude,longitude,elevation,name,country,state
+    meta = MetaData()
+    # station,date,precip,tobs
 
-stations = Table(
-    "stations",
-    meta,
-    Column("id", Integer, primary_key=True),
-    Column("station", String),
-    Column("latitude", Float),
-    Column("longitude", Float),
-    Column("elevation", Float),
-    Column("name", String),
-    Column("country", String),
-    Column("state", String),
-)
+    measure = Table(
+        "measure",
+        meta,
+        Column("id", Integer, primary_key=True),
+        Column("station", String),
+        Column("date", Date),
+        Column("precip", Float),
+        Column("tobs", Integer),
+    )
+    # station,latitude,longitude,elevation,name,country,state
 
-meta.create_all(engine)
-
-conn = engine.raw_connection()
-cursor = conn.cursor()
-
-data_measure = pd.read_csv("clean_measure.csv")
-
-for row in data_measure.itertuples():
-    cursor.execute(
-        """
-                INSERT INTO measure (station, date, precip, tobs)
-                VALUES (?,?,?,?)
-                """,
-        (row.station, row.date, row.precip, row.tobs),
+    stations = Table(
+        "stations",
+        meta,
+        Column("id", Integer, primary_key=True),
+        Column("station", String),
+        Column("latitude", Float),
+        Column("longitude", Float),
+        Column("elevation", Float),
+        Column("name", String),
+        Column("country", String),
+        Column("state", String),
     )
 
+    meta.create_all(engine)
 
-data_stations = pd.read_csv("clean_stations.csv")
+    conn = engine.raw_connection()
+    cursor = conn.cursor()
 
-for row in data_stations.itertuples():
-    cursor.execute(
-        """
-                INSERT INTO stations (station, latitude, longitude, elevation, name, country, state)
-                VALUES (?,?,?,?,?,?,?)
-                """,
-        (
-            row.station,
-            row.latitude,
-            row.longitude,
-            row.elevation,
-            row.name,
-            row.country,
-            row.state,
-        ),
-    )
+    data_measure = pd.read_csv("clean_measure.csv")
 
-conn.commit()
+    for row in data_measure.itertuples():
+        cursor.execute(
+            """
+                    INSERT INTO measure (station, date, precip, tobs)
+                    VALUES (?,?,?,?)
+                    """,
+            (row.station, row.date, row.precip, row.tobs),
+        )
 
-print(conn.execute("SELECT * FROM stations LIMIT 5").fetchall())
+    data_stations = pd.read_csv("clean_stations.csv")
 
+    for row in data_stations.itertuples():
+        cursor.execute(
+            """
+                    INSERT INTO stations (station, latitude, longitude, elevation, name, country, state)
+                    VALUES (?,?,?,?,?,?,?)
+                    """,
+            (
+                row.station,
+                row.latitude,
+                row.longitude,
+                row.elevation,
+                row.name,
+                row.country,
+                row.state,
+            ),
+        )
+
+    conn.commit()
+
+    print(conn.execute("SELECT * FROM stations LIMIT 5").fetchall())
+
+
+if __name__ == "__main__":
+    main()
 # W sumie to moge uzyć pandas i tosql, wtedy nic nie trzeba robić ani definiować Tables ani nic.
 # df.to_sql(measure, con=engine, if_exists='replace', index=False)
