cf122c0a6412607ef37ae43744af72dbf4d856e1
lukacjo <85407127+lukacjo@users.noreply.github.com>
2024-01-03 18:33:47 +0100

    uywanie sqlalchemy i dodanie select i delete

--- zad13_3.py
+++ zad13_3.py
@@ -7,15 +7,26 @@ from sqlalchemy import (
     Table,
     Date,
     Float,
+    delete,
+    select,
 )
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
 import pandas as pd
+
+
+def select_data(session, stmt):
+    result = session.execute(stmt)
+    rows = result.fetchall()
+
+    for row in rows:
+        print(row)
 
 
 def main():
     engine = create_engine("sqlite:///database.db", echo=False)
 
     meta = MetaData()
-    # station,date,precip,tobs
 
     measure = Table(
         "measure",
@@ -26,7 +37,6 @@ def main():
         Column("precip", Float),
         Column("tobs", Integer),
     )
-    # station,latitude,longitude,elevation,name,country,state
 
     stations = Table(
         "stations",
@@ -43,45 +53,57 @@ def main():
 
     meta.create_all(engine)
 
-    conn = engine.raw_connection()
-    cursor = conn.cursor()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
     data_measure = pd.read_csv("clean_measure.csv")
-
     for row in data_measure.itertuples():
-        cursor.execute(
-            """
-                    INSERT INTO measure (station, date, precip, tobs)
-                    VALUES (?,?,?,?)
-                    """,
-            (row.station, row.date, row.precip, row.tobs),
+        session.execute(
+            measure.insert(),
+            {
+                "station": row.station,
+                "date": datetime.strptime(row.date, "%Y-%m-%d").date(),
+                "precip": row.precip,
+                "tobs": row.tobs,
+            },
         )
 
     data_stations = pd.read_csv("clean_stations.csv")
-
     for row in data_stations.itertuples():
-        cursor.execute(
-            """
-                    INSERT INTO stations (station, latitude, longitude, elevation, name, country, state)
-                    VALUES (?,?,?,?,?,?,?)
-                    """,
-            (
-                row.station,
-                row.latitude,
-                row.longitude,
-                row.elevation,
-                row.name,
-                row.country,
-                row.state,
-            ),
+        session.execute(
+            stations.insert(),
+            {
+                "station": row.station,
+                "latitude": row.latitude,
+                "longitude": row.longitude,
+                "elevation": row.elevation,
+                "name": row.name,
+                "country": row.country,
+                "state": row.state,
+            },
         )
 
-    conn.commit()
+    session.commit()
 
-    print(conn.execute("SELECT * FROM stations LIMIT 5").fetchall())
+    stmt = select(stations).limit(5)
+    select_data(session, stmt)
+
+    print()
+
+    stmt = select(stations).where(stations.c.elevation == 3.0)
+    select_data(session, stmt)
+
+    stmt = delete(stations).where(stations.c.elevation == 3.0)
+    session.execute(stmt)
+
+    stmt = delete(stations)
+    session.execute(stmt)
+
+    session.commit()
 
 
 if __name__ == "__main__":
     main()
+
 # W sumie to moge uzyć pandas i tosql, wtedy nic nie trzeba robić ani definiować Tables ani nic.
 # df.to_sql(measure, con=engine, if_exists='replace', index=False)
